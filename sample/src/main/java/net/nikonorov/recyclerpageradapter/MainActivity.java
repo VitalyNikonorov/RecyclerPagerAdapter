a534b3bbb341dc9f7ad4c2f94f355074960cff7b
VitalyNikonorov <nik.vitaly@mail.ru>
2018-01-22 09:48:40 +0300

    add sample

---
+++ sample/src/main/java/net/nikonorov/recyclerpageradapter/MainActivity.java
@@ -0,0 +1,29 @@
+package net.nikonorov.recyclerpageradapter;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Vitaly Nikonorov on 22.01.2018.
+ * email@nikonorov.net
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    private final boolean infiniteAdapter = true;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        ViewPager pager = findViewById(R.id.pager);
+        String[] titles = getResources().getStringArray(R.array.page_titles);
+        MainPagerAdapter pagerAdapter = new MainPagerAdapter(titles, getResources().getIntArray(R.array.page_colors));
+        pagerAdapter.setInfiniteAdapter(infiniteAdapter);
+        pager.setAdapter(pagerAdapter);
+        pager.setCurrentItem(infiniteAdapter ? Integer.MAX_VALUE / 2 - (Integer.MAX_VALUE / 2) % titles.length : 0);
+    }
+
+}
---
+++ sample/src/main/java/net/nikonorov/recyclerpageradapter/MainPagerAdapter.java
@@ -0,0 +1,57 @@
+package net.nikonorov.recyclerpageradapter;
+
+import android.support.annotation.ColorRes;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import net.nikonorov.library.RecyclerPagerAdapter;
+
+/**
+ * Created by Vitaly Nikonorov on 22.01.2018.
+ * email@nikonorov.net
+ */
+
+class MainPagerAdapter extends RecyclerPagerAdapter<MainPagerAdapter.ViewHolder> {
+
+    private final String[] titles;
+    private final @ColorRes
+    int[] colors;
+
+    MainPagerAdapter(String[] titles, int[] colors) {
+        if (colors.length != titles.length) throw new IllegalArgumentException("Titles and colors should be the same number!");
+
+        this.titles = titles;
+        this.colors = colors;
+    }
+
+    @Override
+    public int getDataListSize() {
+        return titles.length;
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        int endlessPosition = isInfiniteAdapter() ? position % titles.length : position;
+        holder.itemView.setBackgroundColor(colors[endlessPosition]);
+        holder.captionView.setText(titles[endlessPosition]);
+    }
+
+    @Override
+    protected ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.pager_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    static class ViewHolder extends RecyclerPagerAdapter.ViewHolder {
+        final TextView captionView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            captionView = itemView.findViewById(R.id.item_number_view);
+        }
+    }
+
+}
